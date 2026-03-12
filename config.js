395782641c65edf209bde1e0881242d031a41610
User <test@test.com>
2026-03-13 00:58:10 +0600

    add config.js to version control for vercel

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: 'https://sbenfjwjzosvhgktgpfl.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_UBcUoMBsEYsJwn9jJGic2g_C5ILzT7d',
+    EID_DATE: '2024-04-10T00:00:00Z' // Update this to the actual Eid date in your timezone
+};
